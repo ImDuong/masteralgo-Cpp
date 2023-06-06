29a60db9f42b17b1d664f494a053a7d4cb90ff2f
Thai Duong Tran <duongtt@cystack.net>
2023-06-06 16:53:27 +0700

    init 520b

---
+++ 520b_two_buttons.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+int main() {
+    int n, m;
+    scanf("%d%d", &n, &m);
+    if (n > m) {
+        printf("%d", n - m);
+        return 0;
+    }
+    
+
+    return 0;
+}
