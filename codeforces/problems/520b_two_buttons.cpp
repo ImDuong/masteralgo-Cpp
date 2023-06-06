1806ea665d5db7735feca0337b46a25e9b752119
LinkYang <shiroyasha699969@gmail.com>
2023-06-07 00:08:49 +0700

    finish 520b

--- codeforces/problems/520b_two_buttons.cpp
+++ codeforces/problems/520b_two_buttons.cpp
@@ -1,13 +1,36 @@
 #include <bits/stdc++.h>
 
-int main() {
+int main()
+{
     int n, m;
     scanf("%d%d", &n, &m);
-    if (n > m) {
+    if (n > m)
+    {
         printf("%d", n - m);
         return 0;
     }
-    
 
+    int nbClicks = 0;
+    while (n != m)
+    {
+        if (m > n)
+        {
+            if (m % 2 == 0)
+            {
+                m /= 2;
+            }
+            else
+            {
+                m++;
+            }
+        }
+        else
+        {
+            m++;
+        }
+        nbClicks++;
+    }
+
+    printf("%d", nbClicks);
     return 0;
 }
